f7989c339136563e9f9d480a33f3e57f7e7ed359
gpontesss <pontes.guisilva@gmail.com>
2019-10-21 17:58:59 -0300

    Add tail source example on fluent.conf

--- fluentd_example/formatters.py
+++ fluentd_example/formatters.py
@@ -1,8 +1,12 @@
+import json
+
 import fluent.handler
+
 
 class StringFluentRecordFormatter(fluent.handler.FluentRecordFormatter):
     """ Formats FluentRecordFormatter dict to a string.
     """
+
     def format(self, record):
         fmtd_dict = super(StringFluentRecordFormatter, self).format(record)
-        return str(fmtd_dict)
+        return json.dumps(fmtd_dict)
